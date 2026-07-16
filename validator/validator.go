486fb115f9ad25540db2836ad4878444c102f79d
0xdonnie7 <ddcollection555@gmail.com>
2026-07-16 22:47:54 +0100

    feat(validator): add validator check

---
+++ validator/validator.go
@@ -0,0 +1,23 @@
+package validator
+
+type Validator struct {
+	errors map[string]string
+}
+
+func NewValidator() *Validator {
+	return &Validator{
+		errors: make(map[string]string),
+	}
+}
+
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.errors[key]; !exists {
+		v.errors[key] = message
+	}
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
