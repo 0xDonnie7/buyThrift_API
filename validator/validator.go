dc99f02a381d6c1052fb7b6e045d4d772f74c1a9
0xdonnie7 <ddcollection555@gmail.com>
2026-07-20 14:39:59 +0100

    feat(validator): add validate email & password functions

--- validator/validator.go
+++ validator/validator.go
@@ -1,5 +1,14 @@
 package validator
 
+import (
+	"regexp"
+	"unicode"
+)
+
+var (
+	EmailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}$")
+)
+
 type Validator struct {
 	errors map[string]string
 }
@@ -20,4 +29,56 @@ func (v *Validator) Check(ok bool, key, message string) {
 	if !ok {
 		v.AddError(key, message)
 	}
+}
+
+func (v *Validator) Errors() map[string]string {
+	return v.errors
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.errors) == 0
+}
+
+func Matches(rx *regexp.Regexp, values string) bool {
+	if rx == nil {
+		return false
+	}
+
+	return rx.MatchString(values)
+}
+
+func (v *Validator) ValidateEmail(email string) {
+	v.Check(email != "", "email", "must not be empty")
+	v.Check(Matches(EmailRegex, email), "email", "email not valid")
+}
+
+func (v *Validator) ValidatePassword(password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be atleast 8 characters")
+	v.Check(len(password) <= 72, "password", "must be atleast 72 characters long")
+	v.Check(StrongPassword(password), "password", "must contain at least one uppercase letter, lowercase letter, number, and special character")
+}
+
+func (v *Validator) ValidateUser(email, password string) {
+	v.ValidateEmail(email)
+	v.ValidatePassword(password)
+}
+
+func StrongPassword(password string) bool {
+	var hasLower, hasUpper, hasDigit, hasSymbol bool
+
+	for _, r := range password {
+		switch {
+		case unicode.IsLower(r):
+			hasLower = true
+		case unicode.IsUpper(r):
+			hasUpper = true
+		case unicode.IsDigit(r):
+			hasDigit = true
+		case unicode.IsSymbol(r) || unicode.IsPunct(r):
+			hasSymbol = true
+		}
+	}
+
+	return hasLower && hasUpper && hasDigit && hasSymbol
 }
