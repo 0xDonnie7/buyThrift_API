82c1e33fe89c560c11c84029189d0065c3d57aa0
0xdonnie7 <ddcollection555@gmail.com>
2026-07-20 22:47:39 +0100

    feat(data): add User struct and Models wrapper

--- internal/data/models.go
+++ internal/data/models.go
@@ -1,10 +1,24 @@
 package data
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
 
 type Models struct {
+	User UserModel
 }
 
 func NewModels(db *sql.DB) Models {
-	return Models{}
+	return Models{
+		User: UserModel{DB: db},
+	}
+}
+
+type User struct {
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"password_hash"`
+	Role         string    `json:"role"`
 }
